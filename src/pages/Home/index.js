cd9aa2196feb2005f93219f3ca2b7f9f753abc56
WalberFellipe <walberfellipe97@gmail.com>
2020-06-23 21:32:24 -0300

    Commit garantia

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../../services/api'
 import './style.css';
 
 export default function Home(){
+    const history = useHistory();
+    const [post,setPost] = useState([]);
+
+    useEffect(() => {
+        api.get('posts').then(Response => {
+            setPost(Response.data)
+
+        })
+    })
     return (
-        <div>
-            <h1>Bem vindo a prova...</h1>
+        <div className="container">            
+             <h1>Bem vindo a prova...</h1>
+             <div className="content">
+              <ul>{
+                  post.map(post => (
+                    <div className='list'>
+                        <li key={post.id}>
+                            <h1>{post.title}</h1>
+                            <h1>{post.body}</h1>
+                        </li>                        
+                    </div>
+                  ))       
+              }</ul>
+            </div>           
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/Home';
+import Comments from './pages/Comments';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Home} />
+                <Route path='/Comments' component={Comments} />
                 <Route path='*' component={() => (<h1>Not Foound!</h1>)} />
             </Switch>
         </BrowserRouter>
